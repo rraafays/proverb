e1ae62f4a364e54a921aa884e4e9aabefff7c061
raf <rraf@tuta.io>
2023-07-30 10:17:55 +0100

    update: made sure that proverbs do not exceed current terminal width

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
-use colored::Colorize;
-use proverb::bestow_wisdom;
-
 mod proverb;
 
+use colored::Colorize;
+use proverb::bestow_wisdom;
+use terminal_size::{terminal_size, Height, Width};
+
 fn main() {
-  println!("{}", bestow_wisdom().white());
+  let dimensions = terminal_size();
+
+  if let Some((Width(w), Height(_h))) = dimensions {
+    let mut proverb: String = bestow_wisdom();
+    while proverb.len() > w as usize {
+      proverb = bestow_wisdom();
+    }
+
+    println!("{}", proverb.white());
+  } else {
+    println!("unable to bestow wisdom")
+  }
 }
