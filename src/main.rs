3a0137e7af7e1cd28341be520b5bb0fd09808321
raf <rraf@tuta.io>
2023-10-18 14:23:39 +0100

    chore: reformatting

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,16 @@ use proverb::bestow_wisdom;
 use terminal_size::{terminal_size, Height, Width};
 
 fn main() {
-  let dimensions = terminal_size();
+    let dimensions = terminal_size();
 
-  if let Some((Width(w), Height(_h))) = dimensions {
-    let mut proverb: String = bestow_wisdom();
-    while proverb.len() > w as usize {
-      proverb = bestow_wisdom();
+    if let Some((Width(w), Height(_h))) = dimensions {
+        let mut proverb: String = bestow_wisdom();
+        while proverb.len() > w as usize {
+            proverb = bestow_wisdom();
+        }
+
+        println!("{}", proverb.white());
+    } else {
+        println!("unable to bestow wisdom")
     }
-
-    println!("{}", proverb.white());
-  } else {
-    println!("unable to bestow wisdom")
-  }
 }
