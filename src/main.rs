e0f222a44616093db36bbfbcd054a9251c1c2643
raf <rraf@tuta.io>
2024-10-26 11:01:28 +0100

    chore: removed size limit

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,7 @@ mod proverb;
 
 use colored::Colorize;
 use proverb::bestow_wisdom;
-use terminal_size::{
-    terminal_size,
-    Height,
-    Width,
-};
 
 fn main() {
-    let dimensions = terminal_size();
-
-    if let Some((Width(w), Height(_h))) = dimensions {
-        let mut proverb: String = bestow_wisdom();
-        while proverb.len() > w as usize {
-            proverb = bestow_wisdom();
-        }
-
-        println!("{}", proverb.white());
-    } else {
-        println!("unable to bestow wisdom")
-    }
+    println!("{}", bestow_wisdom().white());
 }
